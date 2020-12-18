1df5d59351c94073e9782f773cb52b6f8a26db43
Pelmenner <70016123+Pelmenner@users.noreply.github.com>
2020-12-18 14:58:45 +0300

    fixed tokenizer

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -4,11 +4,12 @@ import pymorphy2
 
 
 class Tokenizer:
-    __slots__ = ['_tokenizer', '_unpacker', '_analyzer']
+    __slots__ = ['_tokenizer', '_unpacker', '_analyzer', '_file']
 
     def __init__(self, filename=u"samples/search_items.msgpack"):
+        self._file = open(filename, 'rb')
+        self._unpacker = msgpack.Unpacker(file_like=self._file)
         self._tokenizer = nltk.tokenize.WordPunctTokenizer()
-        self._unpacker = msgpack.Unpacker(file_like=filename)
         self._analyzer = pymorphy2.MorphAnalyzer()
 
     def normalize_token(self, token):
@@ -27,9 +28,14 @@ class Tokenizer:
             result.append(self.normalize_token(w))
         return result
 
-    def token_generator(self, epochs=1):
+    def token_generator(self, epochs=1, print_delta=200):
+        counter1, counter2 = 0, 1
         for epoch in range(epochs):
             for elem in self.generator_from_msgpack():
                 tokens = self.tokenize(elem['content'])
+                counter1 += 1
+                if counter1 == print_delta:
+                    print('generating {} '.format(counter2 * print_delta))
+                    counter1, counter2 = 0, counter2 + 1
                 if tokens:
-                    yield self.normalize_token(tokens)
+                    yield [self.normalize_token(token) for token in tokens]
