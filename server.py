d9be901a573f451c7f63f9963f28aa19f6cd8590
Alexdat2000 <alexdat@list.ru>
2020-12-18 10:39:33 +0300

    added request

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask import request, redirect, make_response
 
 
@@ -10,9 +10,15 @@ def main_page():
     return render_template("main.html")
 
 
-@app.route("/search-request")
+@app.route("/search-request", methods=["GET"])
 def get_results():
+    # обработать запрос request.args["q"]
     return render_template("results.html")
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico')
 
 
 if __name__ == '__main__':
